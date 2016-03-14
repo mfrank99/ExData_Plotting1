b1e820bbe9649dc1af3cbc78f19c6464b87a0f4e
mfrank99 <michael.frank@gatech.edu>
2016-03-14 17:19:47 -0400

    Correct chart label

--- Plot4.R
+++ Plot4.R
@@ -1,20 +1,19 @@
-#Set Working Directory
-#Change this to the location of your unzipped data file
 setwd("C:/Users/Mike/Dropbox/Coursera Exploratory Data Analysis/")
 
 #Read data from file
 powerTemp <- read.table("household_power_consumption.txt",header = TRUE, sep = ";", stringsAsFactors = FALSE, na.strings = "?")
 
-#Convert Date and Time columns to R format
+#Convert Date and Time colums to R format
 powerTemp$Date <- as.Date(powerTemp$Date, "%d/%m/%Y")
-# str(powerTemp)
+str(powerTemp)
 
 #Extract only dates that we are using (Feb 1, 2007 - Feb 2, 2007)
 power <- powerTemp[powerTemp$Date >= "2007-02-01" & powerTemp$Date <= "2007-02-02",]
+#power$DateTime <- strptime(paste(power$Date,power$Time), "%Y-%m-%d %H:%M:%S") #Doesn't Plot!!
 power$DateTime <- as.POSIXct(paste(power$Date,power$Time))
-# str(power)
+str(power)
 
-#Remove unneeded data (Outside of date scope)
+#Cleanup unneeded data
 rm(powerTemp)
 
 #Create Plot4
@@ -28,7 +27,7 @@ par(mfrow = c(2,2))
   plot(power$Voltage~power$DateTime, ylab = "Voltage", xlab = "datetime", type="l")
 
   #plot3
-  plot(power$Sub_metering_1~power$DateTime, ylab = "Global Active Power (kilowatts)", xlab = "", type="n")
+  plot(power$Sub_metering_1~power$DateTime, ylab = "Energy Sub Metering", xlab = "", type="n")
   # legend("topright", cex = 1, bty = "n",y.intersp = .4, lty = c(1,1,1), col = (c("black","blue","red")), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
   legend("topright", bty = "n", lty = c(1,1,1), col = (c("black","blue","red")), legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
   points(power$Sub_metering_1~power$DateTime, type="l")
